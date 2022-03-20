ecdd1a1bcfe225915287c1cf631c54626352f44f
Sally Wright <sallywright@developersinstitute.co.nz>
2022-03-21 12:31:44 +1300

    removed check for :id

--- server/src/idFormat.js
+++ server/src/idFormat.js
@@ -1,11 +1,9 @@
 const idFormat = (formatObject) => {
-  if (!("id" in formatObject)) {
-    return {
-      id: formatObject._id,
-      name: formatObject.name,
-      description: formatObject.description,
-      image: formatObject.image,
-    };
-  }
+  return {
+    id: formatObject._id,
+    name: formatObject.name,
+    description: formatObject.description,
+    image: formatObject.image,
+  };
 };
 module.exports = idFormat;
