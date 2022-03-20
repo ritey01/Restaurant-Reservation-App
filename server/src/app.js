d4de74e588fabd09bd0e816277f53a3a131f3aca
Sally Wright <sallywright@developersinstitute.co.nz>
2022-03-21 12:32:07 +1300

    Added basic GET restaurant endpoint

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const idFormat = require("./idFormat");
 
 app.use(cors());
 app.use(express.json());
+
+app.get("restaurants", async (request, response) => {
+  const restaurants = await ReservationModel.find({});
+  const formatReservations = reservations.map((reservation) => {
+    return reservationFormat(reservation);
+  });
+  response.send(formatReservations).status(200);
+});
 
 module.exports = app;
