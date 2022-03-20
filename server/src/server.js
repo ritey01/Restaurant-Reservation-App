e9acf6535774d9300c34948d822d278e0a7b1d09
Sally Wright <sallywright@developersinstitute.co.nz>
2022-03-21 09:52:09 +1300

    Mongoose connection added

--- server/src/server.js
+++ server/src/server.js
@@ -1,5 +1,8 @@
 const port = process.env.PORT || 5001;
 const app = require("./app");
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/mongo");
 
 app.listen(port, () => {
   console.log(`API server started at http://localhost:${port}`);
