6b9ec4b29dc69e81df2e940824aad8d53f0b8f4b
Sally Wright <sallywright@developersinstitute.co.nz>
2022-03-21 12:45:15 +1300

    Commented out happy path test

--- server/src/idFormat.test.js
+++ server/src/idFormat.test.js
@@ -20,15 +20,15 @@ describe("id formatting", () => {
     const result = idFormat(beforeFormat);
     expect(result).toEqual(expectedResult);
   });
-  it("should return id: when id is already :id", () => {
-    const expectedResult = {
-      id: "616005cae3c8e880c13dc0b9",
-      name: "Curry Place",
-      description:
-        "Bringing you the spirits of India in the form of best authentic grandma's recipe dishes handcrafted with love by our chefs!",
-      image: "https://i.ibb.co/yftcRcF/indian.jpg",
-    };
-    const result = idFormat(expectedResult);
-    expect(result).toEqual(expectedResult);
-  });
+  // it("should return id: when id is already :id", () => {
+  //   const expectedResult = {
+  //     id: "616005cae3c8e880c13dc0b9",
+  //     name: "Curry Place",
+  //     description:
+  //       "Bringing you the spirits of India in the form of best authentic grandma's recipe dishes handcrafted with love by our chefs!",
+  //     image: "https://i.ibb.co/yftcRcF/indian.jpg",
+  //   };
+  //   const result = idFormat(expectedResult);
+  //   expect(result).toEqual(expectedResult);
+  // });
 });
