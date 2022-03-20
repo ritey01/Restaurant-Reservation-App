e0fdb44120cfdd30dbdcf714a2b5ce6c6716991c
Sally Wright <sallywright@developersinstitute.co.nz>
2022-03-21 12:19:55 +1300

    GET restuarants test added

---
+++ server/src/app.test.js
@@ -0,0 +1,36 @@
+const app = require("./app");
+const request = require("supertest");
+
+describe("App", () => {
+  test("GET /restaurants returns all restaurants", async () => {
+    const expected = [
+      {
+        id: "616005cae3c8e880c13dc0b9",
+        name: "Curry Place",
+        description:
+          "Bringing you the spirits of India in the form of best authentic grandma's recipe dishes handcrafted with love by our chefs!",
+        image: "https://i.ibb.co/yftcRcF/indian.jpg",
+      },
+      {
+        id: "616005e26d59890f8f1e619b",
+        name: "Thai Isaan",
+        description:
+          "We offer guests a modern dining experience featuring the authentic taste of Thailand. Food is prepared fresh from quality ingredients and presented with sophisticated elegance in a stunning dining setting filled with all the richness of Thai colour, sound and art.",
+        image: "https://i.ibb.co/HPjd2jR/thai.jpg",
+      },
+      {
+        id: "616bd284bae351bc447ace5b",
+        name: "Italian Feast",
+        description:
+          "From the Italian classics, to our one-of-a-kind delicious Italian favourites, all of our offerings are handcrafted from the finest, freshest ingredients available locally. Whether you're craving Italian comfort food like our Ravioli, Pappardelle or something with a little more Flavour like our famous Fettuccine Carbonara.",
+        image: "https://i.ibb.co/0r7ywJg/italian.jpg",
+      },
+    ];
+    await request(app)
+      .get("/restaurants")
+      .expect(200)
+      .expect((res) => {
+        expect(res.body).toEqual(expected);
+      });
+  });
+});
