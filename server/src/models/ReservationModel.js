da50a981fd3209bc5a819bed718bc3334fbd002c
Sally Wright <sallywright@developersinstitute.co.nz>
2022-03-22 12:19:43 +1300

    Added reservation Model

---
+++ server/src/models/ReservationModel.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const reservationSchema = new Schema({
+  partySize: { type: String, required: true },
+  date: { type: String, required: true },
+  //   userId: { type: String, required: true },
+  restaurantName: { type: String, required: true },
+});
+
+const Reservation = mongoose.model("reservation", reservationSchema);
+
+module.exports = Reservation;
