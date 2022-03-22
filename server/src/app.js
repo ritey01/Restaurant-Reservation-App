93ea42564125ac53eafa0decfc1ea0fa45ef4ea2
Sally Wright <sallywright@developersinstitute.co.nz>
2022-03-23 10:48:45 +1300

    Add celebrate and auth0

--- server/src/app.js
+++ server/src/app.js
@@ -1,26 +1,50 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const idFormat = require("./utils/restaurantFormat");
+const { celebrate, Joi, errors, Segments } = require("celebrate");
+const restaurantFormat = require("./utils/restaurantFormat");
+const reservationFormat = require("./utils/reservationFormat");
 const RestaurantModel = require("./models/RestaurantModel");
 const ReservationModel = require("./models/ReservationModel");
 const validId = require("./utils/validId");
+const { auth } = require("express-oauth2-jwt-bearer");
+const checkJwt = auth({
+  audience: "https://restaurantRes.com",
+  issuerBaseURL: `https://dev-dfkojg63.us.auth0.com/`,
+});
 
 app.use(cors());
 app.use(express.json());
 
-app.post("/reservations", async (request, response) => {
-  const { body } = request;
-  const document = body;
-  const reservation = new ReservationModel(document);
-  await reservation.save();
-  return response.status(201).send(reservation);
-});
+app.post(
+  "/reservations",
+  checkJwt,
+  celebrate({
+    [Segments.BODY]: Joi.object().keys({
+      partySize: Joi.number().min(1).required(),
+      date: Joi.date().greater("now").required(),
+      restaurantName: Joi.string().required(),
+    }),
+  }),
+  async (request, response, next) => {
+    try {
+      const { body, auth } = request;
+      const document = { userId: auth.payload.sub, ...body };
+
+      const reservation = new ReservationModel(document);
+      await reservation.save();
+      return response.status(201).send(reservationFormat(reservation));
+    } catch (error) {
+      error.status = 400;
+      next(error);
+    }
+  }
+);
 app.get("/restaurants", async (request, response, next) => {
   try {
     const restaurants = await RestaurantModel.find({});
     const formatRestaurant = restaurants.map((restaurant) => {
-      return idFormat(restaurant);
+      return restaurantFormat(restaurant);
     });
     response.send(formatRestaurant).status(200);
   } catch (error) {
@@ -37,7 +61,7 @@ app.get("/restaurants/:id", async (request, response, next) => {
   }
   try {
     const restaurant = await RestaurantModel.findById(id);
-    const formatRestaurant = idFormat(restaurant);
+    const formatRestaurant = restaurantFormat(restaurant);
 
     return response.status(200).send(formatRestaurant);
   } catch (error) {
@@ -48,5 +72,5 @@ app.get("/restaurants/:id", async (request, response, next) => {
     }
   }
 });
-
+app.use(errors());
 module.exports = app;
