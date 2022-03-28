606f04da41173ff1b0660de916a0d0a97a3ac0f6
Sally Wright <sallywright@developersinstitute.co.nz>
2022-03-28 16:54:03 +1300

    Edit reservations 404 response

--- server/src/app.js
+++ server/src/app.js
@@ -78,16 +78,15 @@ app.get("/reservations", checkJwt, async (request, response, next) => {
       userId: auth.payload.sub,
     });
 
+    const formatReservations = reservations.map((reservation) => {
+      return reservationFormat(reservation);
+    });
+    response.send(formatReservations).status(200);
     if (reservations.length === 0) {
       return response
         .status(404)
         .send({ error: "You dont have any reservations" });
     }
-
-    const formatReservations = reservations.map((reservation) => {
-      return reservationFormat(reservation);
-    });
-    response.send(formatReservations).status(200);
   } catch (error) {
     next(error);
   }
