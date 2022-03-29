736618394cdbf1899e2ca9939a611ede087f1269
Sally Wright <sallywright@developersinstitute.co.nz>
2022-03-29 13:23:17 +1300

    added 403 reponse

--- server/src/app.js
+++ server/src/app.js
@@ -77,6 +77,9 @@ app.get("/reservations", checkJwt, async (request, response, next) => {
     const reservations = await ReservationModel.find({
       userId: auth.payload.sub,
     });
+    if (request.auth.payload.sub !== reservations.userId) {
+      return response.status(403);
+    }
     if (reservations.length === 0 && request.auth.payload.sub) {
       return response
         .status(404)
