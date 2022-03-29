d53a00f1dff0bf1117d8d8cc576afa12ed383962
Sally Wright <sallywright@developersinstitute.co.nz>
2022-03-30 09:45:24 +1300

    Add enviro variable for mongoDB URI

--- server/src/server.js
+++ server/src/server.js
@@ -2,7 +2,8 @@ const port = process.env.PORT || 5001;
 const app = require("./app");
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/mongo");
+const mongoDbUri = process.env.MONGO_URI || "mongodb://localhost:27017/mongo";
+mongoose.connect(mongoDbUri);
 
 app.listen(port, () => {
   console.log(`API server started at http://localhost:${port}`);
